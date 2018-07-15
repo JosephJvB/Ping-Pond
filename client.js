af415ebf1840596ff881362fbdf9acaabd507d40
JosephJvB <joevanbo@pm.me>
2018-07-15 18:06:59 +1200

    hates a hello world

--- client.js
+++ client.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+import { render } from 'react-dom';
+import h from 'react-hyperscript';
+
+const yaClassic = 'Hello World';
+
+document.addEventListener('DOMContentLoaded', function blastOff() {
+  render (
+    // root component
+    h(App, {
+      yaClassic
+    }),
+    // select dom-element_irl
+    document.getElementById('x-marks-the-spot')
+  );
+});
+
+// console.log('Hello World'); hates it
+
+function App ({yaClassic}) {
+  return h('h1', {}, yaClassic);
+};
